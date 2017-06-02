02f52ef266d5aeae9717dc80257da807ddceb845
Jeff Kempista <jeffkempista@gmail.com>
2017-06-02 15:05:04 -0400

    Adds test for voter delegation

--- test/ballot.js
+++ test/ballot.js
@@ -14,7 +14,7 @@ contract('Ballot', function(accounts) {
 
     return ballot.chairperson.call()
     .then((chairperson) => {
-      assert.equal(chairperson.valueOf(), accounts[0])
+      assert.equal(chairperson.valueOf(), accounts[0], "Expect chairperson to be the contract creater")
     })
   })
 
@@ -22,29 +22,27 @@ contract('Ballot', function(accounts) {
     return ballot.voters.call(accounts[0])
     .then((result) => {
       // weight is the first field in the struct
-      assert.equal(result[0], 1)
+      assert.equal(result[0].toNumber(), 1, "Expect weight to be `1`")
     })
   })
 
   it('should set proposals', () => {
-    return ballot.setProposals([
-      "Meyer", 
-      "O'Brian"
-    ])
+    return ballot.setProposals(["Meyer", "O'Brian"])
     .then(() => {
       return ballot.getProposalsCount.call()
     }).then((count) => {
-      assert.equal(count, 2)
+      console.log('Count: ' + count)
+      assert.equal(count, 2, "Expect 2 proposals to exist")
     }).then(() => {
       return ballot.proposals.call(0)
     }).then((proposal) => {
-      assert.equal(web3.toUtf8(proposal[0]), 'Meyer')
-      assert.equal(proposal[1], 0)
+      assert.equal(web3.toUtf8(proposal[0]), 'Meyer', "Expect name to be `Meyer`")
+      assert.equal(proposal[1].toNumber(), 0, "Expect initial vote count to be `0`")
     }).then(() => {
       return ballot.proposals.call(1)
     }).then((proposal) => {
-      assert.equal(web3.toUtf8(proposal[0]), "O'Brian")
-      assert.equal(proposal[1], 0)
+      assert.equal(web3.toUtf8(proposal[0]), "O'Brian", "Expect name to be `O'Brian`")
+      assert.equal(proposal[1].toNumber(), 0, "Expect initial vote count to be `0`")
     })
   })
 
@@ -53,7 +51,7 @@ contract('Ballot', function(accounts) {
     .then(() => {
       return ballot.voters.call(accounts[1])
     }).then((voter) => {
-      assert.equal(voter[0], 1, 'Expect weight of `1`')
+      assert.equal(voter[0].toNumber(), 1, 'Expect weight of `1`')
       assert.equal(voter[1], false, 'Expect voted to be `false`')
     })
   })
@@ -66,24 +64,48 @@ contract('Ballot', function(accounts) {
       assert.equal(voter[1], true, 'Expect voted to be `true`')
       return ballot.proposals.call(0)
     }).then((proposal) => {
-      assert.equal(proposal[1], 1, 'Expect proposal to have `1` vote')
+      assert.equal(proposal[1].toNumber(), 1, 'Expect proposal to have `1` vote')
       return ballot.giveRightToVote(accounts[2])
     }).then(() => {
       return ballot.vote(0, {from: accounts[2]})
     }).then(() => {
       return ballot.proposals.call(0)
     }).then((proposal) => {
-      assert.equal(proposal[1], 2, 'Expect proposal to have `2` votes')
+      assert.equal(proposal[1].toNumber(), 2, 'Expect proposal to have `2` votes')
       return ballot.giveRightToVote(accounts[3])
     }).then(() => {
       return ballot.vote(1, {from: accounts[3]})
     }).then(() => {
       return ballot.proposals.call(1)
     }).then((proposal) => {
-      assert.equal(proposal[1], 1, 'Expect proposal to have `1` votes')
+      assert.equal(proposal[1].toNumber(), 1, 'Expect proposal to have `1` votes')
       return ballot.winnerName()
     }).then((winnerName) => {
-      assert.equal(web3.toUtf8(winnerName), 'Meyer')
+      assert.equal(web3.toUtf8(winnerName), 'Meyer', "Expect `Meyer` to win")
+    })
+  })
+
+  it('should allow voters to delegate votes', () => {
+
+    let proposalVotes
+
+    return ballot.giveRightToVote(accounts[4])
+    .then(() => {
+      return ballot.giveRightToVote(accounts[5])
+    }).then(() => {
+      return ballot.delegate(accounts[4], {from: accounts[5]})
+    }).then(() => {
+      return ballot.voters(accounts[4])
+    }).then((delegate) => {
+      assert.equal(delegate[0].toNumber(), 2, 'Expected voter weight of delegate to be `2`')
+      return ballot.proposals.call(1)
+    }).then((proposal) => {
+      proposalVotes = proposal[1].toNumber()
+      return ballot.vote(1, {from: accounts[4]})
+    }).then(() => {
+      return ballot.proposals.call(1)
+    }).then((proposal) => {
+      assert.equal(proposal[1].toNumber(), proposalVotes + 2, 'Expected proposal to get 2 votes from delegate')
     })
   })
 
